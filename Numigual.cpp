68deaee3c3acb4f7de6030763da9ffee590302af
EdyMarkL <edymarkllancoa123@gmail.com>
2026-09-14 23:19:08 -0500

    Add files via upload

---
+++ Numigual.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int C1, C2;
+
+    cin >> C1 >> C2;
+
+    if (C1 == C2)
+    {
+        cout << "Iguales";
+    }
+
+    else
+    {
+        cout << "Diferente";
+    }
+
+    return 0;
+}
---
+++ Numposnegneu.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main())
+{
+    int num;
+
+    cin << num;
+
+    IF (num == 0)
+    {
+      cout << "Nulo";
+    }
+
+    else if (num > 0)
+    {
+      cout << "Positivo";
+    }
+    else
+    {
+        cout << "Negativo";
+    }
+
+    return 0;
+}
