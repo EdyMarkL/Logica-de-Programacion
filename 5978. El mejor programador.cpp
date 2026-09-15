b06c685ac6270a860d455a86196042e9408ced51
EdyMarkL <edymarkllancoa123@gmail.com>
2026-09-14 22:17:25 -0500

    Update 5978. El mejor programador.cpp

--- 5978. El mejor programador.cpp
+++ 5978. El mejor programador.cpp
@@ -4,6 +4,21 @@ using namespace std;
 
 int main()
 {
+    string Nombre1, Nombre2;
+    int Puntos1, Puntos2;
+
+    cin >> Nombre1 >> Puntos1;
+    cin >> Nombre2 >> Puntos2;
+
+    if (Puntos1 > Puntos2)
+    {
+        cout << Nombre1 << endl;
+    }
     
+    else if (Puntos2 > Puntos1)
+    {
+        cout << Nombre2 << endl;
+    }
+
     return 0;
 }
