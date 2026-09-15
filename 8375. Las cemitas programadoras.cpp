70be2516ec438c6162a030aa6c44e0549b3cbe8b
EdyMarkL <edymarkllancoa123@gmail.com>
2026-09-14 22:17:11 -0500

    Update 8375. Las cemitas programadoras.cpp

--- 8375. Las cemitas programadoras.cpp
+++ 8375. Las cemitas programadoras.cpp
@@ -4,6 +4,19 @@ using namespace std;
 
 int main()
 {
-    
-    return 0;
+    int Ganancias, Perdidas;
+
+    cin >> Ganancias >> Perdidas;
+
+    if (Ganancias > Perdidas)
+    {
+        cout << "El negocio si va a jalar" << "\n" << Ganancias - Perdidas << endl;
+    }
+
+    else 
+    {
+        cout << "Dejalo ya esta muerto" << "\n" << Perdidas - Ganancias << endl;
+    }
+
+    return 0; 
 }
