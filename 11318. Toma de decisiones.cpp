7a35394f5038f801b011066c9d80f13b232ce350
EdyMarkL <edymarkllancoa123@gmail.com>
2026-09-14 22:18:28 -0500

    Update 11318. Toma de decisiones.cpp

--- 11318. Toma de decisiones.cpp
+++ 11318. Toma de decisiones.cpp
@@ -4,6 +4,31 @@ using namespace std;
 
 int main()
 {
+    int A, B;
+
+    cin >> A >> B;
+
+    if (A + B == 5)
+    {
+        B = B + 3;
+
+        cout << 2*A + B << endl;
+    }
+
+    else
+    {
+        A = A - 1;
+
+        if ((7*A + B) % 2 == 0)
+        {
+            cout << A - B;
+        }
+
+        else
+        {
+            cout << A * B;
+        }
+    }
     
     return 0;
 }
